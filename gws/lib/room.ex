0cc54460d0b042b2fba89fa92e4a39eb77422350
Thomas Cheng <thomascheng81@gmail.com>
2017-12-22 11:27:11 -0500

    Determine a moderator for the room

--- gws/lib/room.ex
+++ gws/lib/room.ex
@@ -2,7 +2,7 @@ defmodule GWS.Room do
   use Agent, restart: :temporary
 
   def start_link(_opts) do
-    Agent.start_link(fn -> %{game: nil, game_state: nil, players: %{}} end)
+    Agent.start_link(fn -> %{game: nil, game_state: nil, players: %{}, moderator: nil} end)
   end
 
   def get_state(room) do
@@ -21,17 +21,46 @@ defmodule GWS.Room do
   end
 
   def add_player(room, player_id, name, channel) do
-    Agent.update(room, &Map.update!(&1, :players, fn ps ->
-      Map.put(ps, player_id, %{ id: player_id, name: name, channel: channel })
-    end))
+    Agent.update(room, fn state ->
+      state
+      |> Map.update!(:players, fn ps ->
+        Map.put(ps, player_id, %{ id: player_id, name: name, channel: channel })
+      end)
+      |> (fn new_state ->
+        if Enum.count(new_state[:players]) == 1 do
+          Map.put(new_state, :moderator, player_id)
+        else
+          new_state
+        end
+      end).()
+    end)
   end
 
   def remove_player_by_channel(room, channel) do
-    Agent.update(room, &Map.update!(&1, :players, fn ps ->
-      ps
-      |> Enum.reject(fn {_k, %{channel: c}} -> c == channel end)
-      |> Enum.into(%{})
-    end))
+    Agent.update(room, fn state ->
+      state
+      |> Map.update!(:players, fn ps ->
+        ps
+        |> Enum.reject(fn {_k, %{channel: c}} -> c == channel end)
+        |> Enum.into(%{})
+      end)
+      |> (fn new_state ->
+        %{players: ps, moderator: m} = new_state
+
+        if !moderator_exists?(ps, m) && Enum.count(ps) > 0 do
+          %{id: next_player_id} = ps
+            |> Map.values
+            |> List.first
+          Map.put(new_state, :moderator, next_player_id)
+        else
+          new_state
+        end
+      end).()
+    end)
+  end
+
+  defp moderator_exists?(players, moderator) do
+    Enum.any?(players, fn {_, p} -> p[:id] == moderator end)
   end
 
   def update_game_state(room, new_state) do
--- gws/test/room_test.exs
+++ gws/test/room_test.exs
@@ -36,11 +36,14 @@ defmodule GWS.RoomTest do
     assert state[:players] == %{}
 
     GWS.Room.add_player(room, "player-id-1", "Harold", 1)
+    GWS.Room.add_player(room, "player-id-2", "Bob", 2)
     {:ok, state} = GWS.Room.get_state(room)
 
     assert state[:players] == %{
-      "player-id-1" => %{id: "player-id-1", name: "Harold"}
+      "player-id-1" => %{id: "player-id-1", name: "Harold"},
+      "player-id-2" => %{id: "player-id-2", name: "Bob"}
     }
+    assert state[:moderator] == "player-id-1"
   end
 
   test "does not duplicate existing player", %{room: room} do
@@ -60,6 +63,16 @@ defmodule GWS.RoomTest do
     {:ok, state} = GWS.Room.get_state(room)
 
     assert state[:players] == %{}
+  end
+
+  test "reassigns moderator when moderator leaves", %{room: room} do
+    GWS.Room.add_player(room, "player-id-1", "Harold", 1)
+    GWS.Room.add_player(room, "player-id-2", "Bob", 2)
+    GWS.Room.remove_player_by_channel(room, 1)
+
+    {:ok, state} = GWS.Room.get_state(room)
+
+    assert state[:moderator] == "player-id-2"
   end
 
   test "removes a room", %{room: room} do
