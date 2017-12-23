f8179fb04b6f68e493612316976a748f49b61aaa
Thomas Cheng <thomascheng81@gmail.com>
2017-12-23 13:02:28 -0500

    Sort and add odds after all guesses are in

--- you_bet/lib/you_bet.ex
+++ you_bet/lib/you_bet.ex
@@ -7,26 +7,73 @@ defmodule YouBet do
       stage: :guessing,
       question: "how much?",
       players: players
-        |> Enum.map(fn {id, _} -> {id, %{id: id, guess: nil, score: 200}} end)
+        |> Enum.map(fn {id, _} -> {id, %{id: id, guess: nil, bet: nil, score: 200}} end)
         |> Enum.into(%{})
     }
   end
 
-  def sanitize_state(state) do
+  def sanitize_state(%{stage: stage} = state) do
     state
-    |> update_all_players(fn %{guess: guess} = player ->
-      player
-      |> Map.put(:guessed, !is_nil(guess))
-      |> Map.drop([:guess])
+    |> add_guesses_if_betting
+    |> update_all_players(fn %{guess: guess, bet: bet} = player ->
+      if stage == :guessing do
+        player
+        |> Map.put(:guessed, !is_nil(guess))
+        |> Map.drop([:guess, :bet])
+      else
+        player
+        |> Map.put(:bet, !is_nil(bet))
+        |> Map.drop([:guess])
+      end
     end)
   end
 
   def play(state, player_id, "guess", payload) do
     state
-    |> update_player(player_id, &Map.put(&1, :guess, payload))
+    |> update_player(player_id, &Map.put(&1, :guess, elem(Integer.parse(payload), 0)))
+    |> update_if_all_guesses_in
   end
   def play(state, _, _, _), do: state
   def play(state, _, _), do: state
+
+  defp update_if_all_guesses_in(%{players: players} = state) do
+    if Enum.all?(players, fn {_, %{guess: guess}} -> !is_nil(guess) end) do
+      Map.put(state, :stage, :betting)
+    else
+      state
+    end
+  end
+
+  defp add_guesses_if_betting(%{stage: stage, players: players} = state) do
+    if stage == :betting do
+      state
+      |> Map.put(:guesses, process_guesses(players))
+    else
+      state
+    end
+  end
+
+  defp process_guesses(players) do
+    players
+    |> Enum.group_by(fn {_, %{guess: guess}} -> guess end, fn {id, _} -> id end)
+    |> Enum.sort_by(fn {guess, _} -> guess end)
+    |> Enum.map(fn {guess, player_ids} ->
+      %{guess: guess, players: player_ids}
+    end)
+    |> add_odds
+  end
+
+  defp add_odds(guesses) do
+    num_guesses = Enum.count(guesses)
+    mid_point = num_guesses / 2 - 0.5
+    base = if rem(num_guesses, 2) == 0, do: 2.5, else: 2
+
+    guesses
+    |> Enum.with_index
+    |> Enum.map(fn {guess, index} ->
+      Map.put(guess, :odds, Float.round(abs(index - mid_point) + base))
+    end)
+  end
 
   defp update_all_players(state, transformation) do
     state
--- you_bet/test/you_bet_test.exs
+++ you_bet/test/you_bet_test.exs
@@ -70,4 +70,55 @@ defmodule YouBetTest do
       }
     }
   end
+
+  test "sets stage to betting when all guesses are in", %{players: players} do
+    state =
+      players
+      |> YouBet.initial_state
+      |> YouBet.play("1", "guess", "30")
+      |> YouBet.play("2", "guess", "20")
+      |> YouBet.play("3", "guess", "10")
+      |> YouBet.sanitize_state
+
+    assert state == %{
+      round: 1,
+      stage: :betting,
+      question: "how much?",
+      players: %{
+        "1" => %{id: "1", score: 200, bet: false},
+        "2" => %{id: "2", score: 200, bet: false},
+        "3" => %{id: "3", score: 200, bet: false}
+      },
+      guesses: [
+        %{guess: 10, odds: 3, players: ["3"]},
+        %{guess: 20, odds: 2, players: ["2"]},
+        %{guess: 30, odds: 3, players: ["1"]}
+      ]
+    }
+  end
+
+  test "sets correct odds for even number of guesses", %{players: players} do
+    state =
+      players
+      |> YouBet.initial_state
+      |> YouBet.play("1", "guess", "20")
+      |> YouBet.play("2", "guess", "20")
+      |> YouBet.play("3", "guess", "10")
+      |> YouBet.sanitize_state
+
+    assert state == %{
+      round: 1,
+      stage: :betting,
+      question: "how much?",
+      players: %{
+        "1" => %{id: "1", score: 200, bet: false},
+        "2" => %{id: "2", score: 200, bet: false},
+        "3" => %{id: "3", score: 200, bet: false}
+      },
+      guesses: [
+        %{guess: 10, odds: 3, players: ["3"]},
+        %{guess: 20, odds: 3, players: ["1", "2"]},
+      ]
+    }
+  end
 end
