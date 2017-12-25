bfd0b155e23e68ada45c33af2608a74dfec8549a
Thomas Cheng <thomascheng81@gmail.com>
2017-12-25 16:15:23 -0500

    Add mock state for betting stage

--- frontend/src/utils/mockData.js
+++ frontend/src/utils/mockData.js
@@ -1,4 +1,4 @@
-export const FAKE_STATE = {
+export const GUESSING_STATE = {
   roomCode: "FOOB",
   roomJoined: true,
   yourId: "1",
@@ -18,5 +18,32 @@ export const FAKE_STATE = {
     question: "How many roads must a man walk down?",
     round: 1,
     stage: "guessing"
+  }
+};
+
+export const BETTING_STATE = {
+  roomCode: "FOOB",
+  roomJoined: true,
+  yourId: "1",
+  you: {
+    id: "1",
+    name: "Foo"
+  },
+  others: [{ id: "2", name: "Bar" }, { id: "3", name: "Baz" }],
+  gameId: "you_bet",
+  playersNeeded: 3,
+  gameState: {
+    you: { id: "1", name: "Foo", bet: null },
+    others: [
+      { id: "2", name: "Bar", bet: null },
+      { id: "3", name: "Baz", bet: null }
+    ],
+    question: "How many roads must a man walk down?",
+    round: 1,
+    stage: "betting",
+    guesses: [
+      { guess: 3, odds: 3, players: ["Foo", "Bar"]},
+      { guess: 5, odds: 3, players: ["Baz"]}
+    ]
   }
 };
