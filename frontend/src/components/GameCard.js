fc885da713e3efc217968a9e4cb38852214bfe77
Thomas Cheng <thomascheng81@gmail.com>
2017-12-25 01:01:59 -0500

    Make join game section fill entire space

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -2,10 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Button from "./common/Button";
 
-const Container = styled.div`
-  display: flex;
-`;
-
 const FootNote = styled.div`
   margin-top: 4px;
   color: rgba(255,255,255,0.4);
@@ -19,16 +15,14 @@ const ActionContainer = styled.div`
 `;
 
 const GameCard = ({ title, description, playerRequirements, onSelect }) => (
-  <Container>
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ActionContainer>
-        <Button onClick={onSelect}>Start Game</Button>
-        <FootNote>{playerRequirements}</FootNote>
-      </ActionContainer>
-    </div>
-  </Container>
+  <div>
+    <h1>{title}</h1>
+    <p>{description}</p>
+    <ActionContainer>
+      <Button onClick={onSelect}>Start Game</Button>
+      <FootNote>{playerRequirements}</FootNote>
+    </ActionContainer>
+  </div>
 );
 
 export default GameCard;
--- frontend/src/components/common/TextInput.js
+++ frontend/src/components/common/TextInput.js
@@ -1,16 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 5px;
-`;
-
 const Input = styled.input`
   display: block;
   width: 100%;
-  background-color: rgba(0,0,0,0.3);
-  border: 1px solid rgba(0,0,0,0.2);
+  background-color: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgba(0, 0, 0, 0.2);
   font-size: 24px;
   font-family: Roboto, sans-serif;
   color: #fff;
@@ -19,11 +14,6 @@ const Input = styled.input`
   padding: 0 15px;
 `;
 
-const TextInput = ({ label, name, ...props }) => (
-  <div>
-    {label && <Label htmlFor={name}>{label}</Label>}
-    <Input {...props} name={name} type="text" />
-  </div>
-);
+const TextInput = props => <Input {...props} type="text" />;
 
 export default TextInput;
