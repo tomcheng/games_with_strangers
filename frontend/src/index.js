87b3058b2ddfcb0985be769885d4e14e8b510de3
Thomas Cheng <thomascheng81@gmail.com>
2018-01-01 15:01:45 -0500

    Remove service worker since it’s not being served through https to start

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppContainer from "./components/AppContainer";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <AppContainer />,
   document.getElementById("root")
 );
 
-registerServiceWorker();
+// registerServiceWorker();
