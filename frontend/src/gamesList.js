40b451556d8f40cce6e7e9929157b7e399f62441
Thomas Cheng <thomascheng81@gmail.com>
2018-01-07 12:50:18 -0500

    Reorder games

--- frontend/src/gamesList.js
+++ frontend/src/gamesList.js
@@ -3,16 +3,17 @@ import FunPrompts from "./components/games/funPrompts/FunPrompts";
 
 const gamesList = [
   {
+    id: "fun_prompts",
+    displayName: "Fun Prompts",
+    description:
+      "Write funny responses to prompts and vote on which responses you like best.",
+    component: FunPrompts
+  },
+  {
     id: "you_bet",
     displayName: "You Bet",
     description: "Answer general trivia questions and wager on guesses.",
     component: YouBet
-  },
-  {
-    id: "fun_prompts",
-    displayName: "Fun Prompts",
-    description: "Write funny responses to prompts and vote on which responses you like best.",
-    component: FunPrompts
   }
 ];
 
