96f42656121c630198e7010e8201ec61ba74c2da
Thomas Cheng <thomascheng81@gmail.com>
2018-01-09 00:09:29 -0500

    Replace dumb quotes with apostrophes

--- frontend/src/components/common/Heading.js
+++ frontend/src/components/common/Heading.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import Spacing from "./Spacing";
 
+const processString = str => str.replace(/'/g, "’");
+
 const Heading = ({ children, level, spaceBottom, spaceTop, center }) => {
   const style = { textAlign: center ? "center" : null };
   const Comp = "h" + level;
@@ -9,14 +11,14 @@ const Heading = ({ children, level, spaceBottom, spaceTop, center }) => {
   return (
     <Spacing spaceBottom={spaceBottom} spaceTop={spaceTop}>
       {({ spacingStyle }) => (
-        <Comp style={{ ...spacingStyle, ...style }}>{children}</Comp>
+        <Comp style={{ ...spacingStyle, ...style }}>{processString(children)}</Comp>
       )}
     </Spacing>
   );
 };
 
 Heading.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.string.isRequired,
   center: PropTypes.bool,
   level: PropTypes.oneOf([1, 3]),
   spaceBottom: PropTypes.number,
