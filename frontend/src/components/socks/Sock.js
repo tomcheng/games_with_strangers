45c236bda32a1f9685b245487577b92b1a406214
Thomas Cheng <thomascheng81@gmail.com>
2018-10-06 19:06:48 -0400

    Add animation to hand selection

--- frontend/src/components/socks/Sock.js
+++ frontend/src/components/socks/Sock.js
@@ -8,11 +8,24 @@ import Smell from "./Smell";
 import Hand from "./Hand";
 
 const ANGLE_OVERSHOOT = 15;
+const HAND_MIDDLE_X = 16;
+const HAND_MIDDLE_Y = 15;
+const DISTANCE_BUFFER = 50;
 
 const TRANSLATIONS = {
-  1: "3px, 40px",
-  2: "0px, 58px",
-  3: "-4px, 80px"
+  1: { x: 3, y: 40 },
+  2: { x: 0, y: 58 },
+  3: { x: -4, y: 80 }
+};
+
+const getTranslation = ({ length, showHand, handAngle, handDistance }) => {
+  const { x, y } = TRANSLATIONS[length];
+  if (showHand) {
+    return `${x}px, ${y}px`;
+  }
+
+  return `${x - handDistance * Math.sin((handAngle * Math.PI) / 180)}px, ${y +
+  handDistance * Math.cos((handAngle * Math.PI) / 180)}px`;
 };
 
 const Container = styled.div`
@@ -25,7 +38,8 @@ const Container = styled.div`
 const HandContainer = styled.div`
   position: absolute;
   z-index: 1;
-  transform-origin: 16px 15px;
+  transform-origin: ${HAND_MIDDLE_X}px ${HAND_MIDDLE_Y}px;
+  transition: transform 0.12s ease-out;
 `;
 
 class Sock extends Component {
@@ -47,29 +61,40 @@ class Sock extends Component {
     super();
 
     const { x, y } = props.position;
+    const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
     let handAngle = 0;
+    let handDistance = 0;
 
     if (x === 0 && y === 0) {
       handAngle = random(90 - ANGLE_OVERSHOOT, 180 + ANGLE_OVERSHOOT);
+      handDistance = Math.max(windowWidth, windowHeight)/4;
     } else if (x === 1 && y === 0) {
       handAngle = random(180 - ANGLE_OVERSHOOT, 180 + ANGLE_OVERSHOOT);
+      handDistance = windowHeight/4;
     } else if (x === 2 && y === 0) {
       handAngle = random(180 - ANGLE_OVERSHOOT, 270 + ANGLE_OVERSHOOT);
+      handDistance = Math.max(windowWidth, windowHeight)/4;
     } else if (x === 0 && y === 1) {
       handAngle = random(90 - ANGLE_OVERSHOOT, 90 + ANGLE_OVERSHOOT);
+      handDistance = windowWidth/4;
     } else if (x === 1 && y === 1) {
       handAngle = sample([60, 120, 240, 300]);
+      handDistance = windowWidth * 0.6;
     } else if (x === 2 && y === 1) {
       handAngle = random(270 - ANGLE_OVERSHOOT, 270 + ANGLE_OVERSHOOT);
+      handDistance = windowWidth/4;
     } else if (x === 0 && y === 2) {
       handAngle = random(-ANGLE_OVERSHOOT, 90 + ANGLE_OVERSHOOT);
-    } else if (1 === 0 && y === 2) {
+      handDistance = Math.max(windowWidth, windowHeight)/4;
+    } else if (x === 1 && y === 2) {
       handAngle = random(-ANGLE_OVERSHOOT, ANGLE_OVERSHOOT);
-    } else if (2 === 0 && y === 2) {
+      handDistance = windowHeight/4;
+    } else if (x === 2 && y === 2) {
       handAngle = random(270 - ANGLE_OVERSHOOT, 360 + ANGLE_OVERSHOOT);
+      handDistance = Math.max(windowWidth, windowHeight)/4;
     }
 
-    this.state = { handAngle };
+    this.state = { handAngle, handDistance: handDistance + DISTANCE_BUFFER };
   }
 
   render() {
@@ -82,23 +107,24 @@ class Sock extends Component {
       otherSelected,
       onClick
     } = this.props;
-    const { handAngle } = this.state;
+    const { handAngle, handDistance } = this.state;
 
     return (
       <Container onClick={onClick}>
         <Smell smell={smell} length={length} />
         <SockDrawing color={color} length={length} pattern={pattern} />
-        {(youSelected || otherSelected) && (
-          <HandContainer
-            style={{
-              transform: `translate3d(${
-                TRANSLATIONS[length]
-              }, 0) rotate3d(0,0,1,${handAngle}deg)`
-            }}
-          >
-            <Hand />
-          </HandContainer>
-        )}
+        <HandContainer
+          style={{
+            transform: `translate3d(${getTranslation({
+              showHand: youSelected || otherSelected,
+              length,
+              handAngle,
+              handDistance
+            })}, 0) rotate3d(0,0,1,${handAngle}deg)`
+          }}
+        >
+          <Hand />
+        </HandContainer>
       </Container>
     );
   }
