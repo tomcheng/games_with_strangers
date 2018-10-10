755ea8f9bbe535bb8bc55f59d0bd8cbef9ca1eff
Thomas Cheng <thomascheng81@gmail.com>
2018-10-10 18:47:07 -0400

    Add Object.entries polyfill

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,20 @@ import { ThemeProvider } from "styled-components";
 import AppContainer from "./components/AppContainer";
 // import registerServiceWorker from "./registerServiceWorker";
 
+if (!Object.entries) {
+  Object.entries = function(obj) {
+    const ownProps = Object.keys(obj);
+    let i = ownProps.length;
+    const resArray = new Array(i); // preallocate the Array
+    while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    return resArray;
+  };
+}
+
 render(
-  <ThemeProvider theme={{ appPadding: 24 }}><AppContainer /></ThemeProvider>,
+  <ThemeProvider theme={{ appPadding: 24 }}>
+    <AppContainer />
+  </ThemeProvider>,
   document.getElementById("root")
 );
 
